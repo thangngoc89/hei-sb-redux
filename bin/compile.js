20ac383e0ab43ef09597de0621c0ceaa124c8814
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-19 00:54:49 +0700

    fix build

--- bin/compile.js
+++ bin/compile.js
@@ -17,12 +17,12 @@ compiler.run(function (err, stats) {
     process.exit(1)
   } else if (jsonStats.errors.length > 0) {
     debug('Webpack compiler encountered errors.')
-    // process.exit(1)
+    process.exit(1)
   } else if (jsonStats.warnings.length > 0) {
     debug('Webpack compiler encountered warnings.')
 
     if (config.compiler_fail_on_warning) {
-      // process.exit(1)
+      process.exit(1)
     }
   } else {
     debug('No errors / warnings encountered.')
