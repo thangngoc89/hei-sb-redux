a1f4443349807ca6c7b50ecb1e0c91026615a66f
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-18 18:52:55 +0700

    ui(LoginView): Minor layout fix

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -3,18 +3,19 @@ import { Link } from 'react-router'
 import CSSModules from 'react-css-modules'
 import LoginForm from 'components/LoginForm'
 import styles from './LoginView.scss'
-import { Row, Grid } from 'react-bootstrap'
+import { Col, Row, Grid } from 'react-bootstrap'
+
 export class LoginView extends React.Component {
   render () {
     return (
       <Grid fluid>
         <Row>
-          <div className='col-xs-10 col-md-5 center-block'>
+          <Col xs={11} md={5} className='center-block'>
             <div styleName='main-container'>
               <LoginForm />
             </div>
             <Link to='/'>&#8592; return home</Link>
-          </div>
+          </Col>
         </Row>
       </Grid>
     )
