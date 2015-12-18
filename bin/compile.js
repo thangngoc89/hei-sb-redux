9e825c82af67d8b2f8837baa37c440f3262b13e3
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-19 00:47:15 +0700

    chore: Remove unneeded console.log

--- bin/compile.js
+++ bin/compile.js
@@ -11,7 +11,6 @@ compiler.run(function (err, stats) {
   const jsonStats = stats.toJson()
 
   debug('Webpack compile completed.')
-  console.log(stats.toString(config.compiler_stats))
 
   if (err) {
     debug('Webpack compiler encountered a fatal error.', err)
