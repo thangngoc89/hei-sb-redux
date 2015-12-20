d4f7f3e693aac01bb1302bc3e42b418af3d453a5
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-20 08:17:53 +0700

    chore(clean): Remove HomeView from starter kit

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import CoreLayout from 'layouts/CoreLayout'
-import HomeView from 'views/HomeView'
 import LoginView from 'views/LoginView'
 import RuleView from 'views/RuleView'
 import QuizView from 'views/QuizView'
@@ -11,7 +10,6 @@ export default (
   <Route component={CoreLayout} path='/'>
     <IndexRoute component={RuleView} />
     <Route component={LoginView} path='/login' />
-    <Route component={HomeView} path='/home' />
     <Route component={QuizView} path='/quiz' />
   </Route>
 )
