b5aaeba3cb934a133a534e7d5f817af3fb72397d
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-23 21:48:27 +0700

    chore(clean): Remove old components

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,7 +4,6 @@ import CoreLayout from 'layouts/CoreLayout'
 import LoginView from 'views/LoginView'
 import RuleView from 'views/RuleView'
 import QuizView from 'views/QuizView'
-import TimerView from 'views/TimerView'
 
 // TODO: Protect QuizView
 export default (
@@ -12,6 +11,5 @@ export default (
     <IndexRoute component={RuleView} />
     <Route component={LoginView} path='/login' />
     <Route component={QuizView} path='/quiz' />
-    <Route component={TimerView} path='/timer' />
   </Route>
 )
