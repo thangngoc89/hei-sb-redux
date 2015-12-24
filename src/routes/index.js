7942d830b7f54182fce6649d788afb30b1648c59
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-25 04:06:39 +0700

    feature(route): Add Protected route
    Expose store from app.js
    Use React-router onEnter hook

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,27 @@ import CoreLayout from 'layouts/CoreLayout'
 import LoginView from 'views/LoginView'
 import RuleView from 'views/RuleView'
 import QuizView from 'views/QuizView'
+import { store } from 'app'
 
-// TODO: Protect QuizView
+const requireLogin = (nextState, replaceState, next) => {
+  if (__DEV__) {
+    next()
+  }
+  
+  const {user: { code, contestantId }} = store.getState()
+  if (!code || !contestantId) {
+    replaceState(null, '/')
+  }
+  next()
+}
+
 export default (
   <Route component={CoreLayout} path='/'>
     <IndexRoute component={RuleView} />
     <Route component={LoginView} path='/login' />
-    <Route component={QuizView} path='/quiz' />
+
+    <Route onEnter={requireLogin}>
+      <Route component={QuizView} path='/quiz' />
+    </Route>
   </Route>
 )
