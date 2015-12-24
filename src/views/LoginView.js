227cc43cd6bef64ac4097a1aede46f6a6d0a69ec
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-25 04:26:21 +0700

    refactor(cleanup): stateless component
    - Remove QuizInstruction component (useless)
    - Extract these view component into stateless
      + RuleView
      + LoginView

--- src/views/LoginView.js
+++ src/views/LoginView.js
@@ -3,16 +3,14 @@ import { Link } from 'react-router'
 import LoginForm from 'components/LoginForm'
 import Container from 'layouts/TransparentContainerLayout'
 
-export class LoginView extends React.Component {
-  render () {
-    let outsideContainer = (<Link to='/'>&#8592; return home</Link>)
+const outsideContainer = (<Link to='/'>&#8592; return home</Link>)
 
-    return (
-      <Container xs={12} sm={10} md={8} lg={6} outside={outsideContainer}>
-        <LoginForm />
-      </Container>
-    )
-  }
+const LoginView = () => {
+  return (
+    <Container xs={12} sm={10} md={8} lg={6} outside={outsideContainer}>
+      <LoginForm />
+    </Container>
+  )
 }
 
 export default LoginView
