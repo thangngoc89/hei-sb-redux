7a49946e2bbbaa0aaecfa2470a34ec75e046212f
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-25 04:12:53 +0700

    chore(route): Add a 404 Not Found route

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,13 +4,14 @@ import CoreLayout from 'layouts/CoreLayout'
 import LoginView from 'views/LoginView'
 import RuleView from 'views/RuleView'
 import QuizView from 'views/QuizView'
+import NotFound from 'views/NotFound'
 import { store } from 'app'
 
 const requireLogin = (nextState, replaceState, next) => {
   if (__DEV__) {
     next()
   }
-  
+
   const {user: { code, contestantId }} = store.getState()
   if (!code || !contestantId) {
     replaceState(null, '/')
@@ -23,8 +24,12 @@ export default (
     <IndexRoute component={RuleView} />
     <Route component={LoginView} path='/login' />
 
+    { /* Protected routes */ }
     <Route onEnter={requireLogin}>
       <Route component={QuizView} path='/quiz' />
     </Route>
+
+    { /* Catch all route */ }
+    <Route path='*' component={NotFound} status={404} />
   </Route>
 )
