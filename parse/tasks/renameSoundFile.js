a879b17b977916f1d4100679f90e39d732d1e616
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-25 05:12:37 +0700

    task(soundFiles): Write rename script
    Remove the postfix of each audio file

---
+++ parse/tasks/renameSoundFile.js
@@ -0,0 +1,53 @@
+var _path = require('path')
+var fs = require('fs')
+var Parse = require('parse/node')
+Parse.initialize('wUyaZGM0qPNvr2DvKOgGTJSPXa1GWcHV3v3otEiX', 'UuxpC6qz6NeU8pauVnzZ7gp9mViPMR3UeUx9K4Fd')
+// Get sould file path from word
+// basically start of file name
+var getSoundFilePath = function (word) {
+  var regex = new RegExp('^(' + word + ')')
+
+  var path = _path.join(__dirname, 'data/sound')
+  var files = fs.readdirSync(path)
+
+  for (var i = 0; i < files.length; i++) {
+    if (regex.test(files[i])) {
+      var returnPath = _path.join(__dirname, 'data/sound', files[i])
+      return returnPath
+    }
+  }
+  throw new Error('Cannot find ' + word + ' audio file')
+}
+
+var WordObject = Parse.Object.extend('Word')
+var query = new Parse.Query(WordObject)
+query.limit(200)
+query.find().then(
+  function (res) {
+    if (res.length > 0) {
+      renameFile(res)
+    }
+  },
+  function (err) {
+    console.error('Nah! Error while getting words')
+    console.error(err)
+  })
+
+var renameFile = function (res) {
+  for (var i = 0; i < res.length; i++) {
+    doRenameFile(res[i])
+  }
+}
+
+var doRenameFile = function (word) {
+  word = word.get('word') // Remove parse object
+  var oldPath = getSoundFilePath(word)
+  if (typeof oldPath !== 'string') {
+    return
+  }
+
+  var newPath = _path.join(__dirname, 'data/sound', word + '.mp3')
+  fs.rename(oldPath, newPath, function () {
+    console.log('Renamed :' + word + '.mp3')
+  })
+}
