7e5c75a1d47c0ab84e16459b2d9971c663e7ab19
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-25 22:19:16 +0700

    first working code

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -1,7 +1,9 @@
 var wordList = require('cloud/functions/wordList')
 var checkCode = require('cloud/functions/checkCode')
+var sendAnswers = require('cloud/functions/sendAnswers')
 
 Parse.Cloud.useMasterKey()
 
 Parse.Cloud.define('checkCode', checkCode)
 Parse.Cloud.define('wordList', wordList)
+Parse.Cloud.define('sendAnswers', sendAnswers)
