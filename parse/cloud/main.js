c0f719500576633e6d47ccdf446aeadef47557b6
Khoa Nguyen <khoa02031995@gmail.com>
2015-12-28 02:53:58 +0700

    parse(Code): Generate custom code

--- parse/cloud/main.js
+++ parse/cloud/main.js
@@ -7,3 +7,37 @@ Parse.Cloud.useMasterKey()
 Parse.Cloud.define('checkCode', checkCode)
 Parse.Cloud.define('wordList', wordList)
 Parse.Cloud.define('sendAnswers', sendAnswers)
+
+// Check for duplicate code before save
+Parse.Cloud.beforeSave('Code', function(req, res) {
+  // If this is a update operator
+  if (!req.object.isNew()) {
+    res.success()
+    return
+  }
+
+  if (req.object.get('code') === undefined) {
+    res.error('Code cannot be undefined')
+    return
+  }
+
+  if (req.object.get('code').length !== 10) {
+    res.error('Code has to have 10 characters')
+    return
+  }
+
+  var query = new Parse.Query('Code')
+  query.equalTo('code', req.object.get('code'))
+  query.find()
+  .then(function (result) {
+    if (result.length !== 0) {
+      return Parse.Promise.error('Duplicate code')
+    }
+  })
+  .then(function () {
+    res.success()
+  })
+  .then(null, function () {
+    res.error('Duplicate code')
+  })
+})
