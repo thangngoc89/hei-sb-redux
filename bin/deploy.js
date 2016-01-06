e8eb19aff6d538d362ec3f08838081460e1871eb
Khoa Nguyen <khoa02031995@gmail.com>
2016-01-06 20:31:00 +0700

    deploy: Allow deploy locally

--- bin/deploy.js
+++ bin/deploy.js
@@ -5,11 +5,24 @@ const debug = require('debug')('app:bin:deploy')
 const path = require('path')
 
 debug('Start deployment process.')
+
+debug('Clean up temp folder')
 ghpages.clean()
-ghpages.publish(path.join(__dirname, '../dist'), {
-  repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/thangngoc89/hei-sb-redux.git',
-  silent: true
-}, callback)
+
+if (!process.env.GITHUB_TOKEN) {
+  debug('Deploy with SSH')
+  ghpages.publish(path.join(__dirname, '../dist'), {
+    logger: function (message) {
+      debug(message)
+    }
+  }, callback)
+} else {
+  debug('Deploy with github token')
+  ghpages.publish(path.join(__dirname, '../dist'), {
+    repo: 'https://' + process.env.GITHUB_TOKEN + '@github.com/thangngoc89/hei-sb-redux.git',
+    silent: true
+  }, callback)
+}
 
 var callback = function (err) {
   if (err) {
