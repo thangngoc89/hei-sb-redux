d422c2c5d901daf8416217e2bcb241dfb351b007
Khoa Nguyen <khoa02031995@gmail.com>
2016-01-06 19:32:18 +0700

    feat(Footer): Clickable sponsor's image with tooltip

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,4 +1,21 @@
-import sponsors from 'assets/img/ntt.png'
+import logos from './style.scss'
+import Tooltip from 'react-tooltip'
+import sponsorsList from './sponsorsList'
+
+const Logo = ({name, cssClass, link}) => {
+  let object = <span data-tip data-for={cssClass} className={logos[cssClass]} />
+  if (link) {
+    object = <a href={link} target='_blank' data-tip data-for={cssClass} className={logos[cssClass]} />
+  }
+  return (
+    <div key={cssClass} className={logos.div}>
+      {object}
+      <Tooltip id={cssClass}>
+        <span>{name}</span>
+      </Tooltip>
+    </div>
+  )
+}
 
 const Footer = () => {
   return (
@@ -7,7 +24,11 @@ const Footer = () => {
         <p className='text-muted center-block'>
           Sponsored by
         </p>
-        <img src={sponsors} style={{maxWidth: '100%', height: 'auto'}} />
+        <div className='row'>
+          <div className='col-sm-12'>
+            {sponsorsList.map(Logo)}
+          </div>
+        </div>
         <p className='text-muted'>
           Handcrafted with <i className='fa fa-heart' style={{color: '#F00'}} />
           &nbsp;by <a href='http://khoanguyen.me' target='_blank'>Khoa Nguyen</a>
---
+++ src/components/Footer/sponsorsList.js
@@ -0,0 +1,42 @@
+const data = [
+  {
+    'name': 'Đoàn Thanh niên Cộng sản Hồ Chí Minh',
+    'cssClass': 'doan'
+  },
+  {
+    'name': 'Hội Sinh Viên Việt Nam',
+    'cssClass': 'hsv'
+  },
+  {
+    'name': 'University of Medicine and Pharmacy, Ho Chi Minh City',
+    'cssClass': 'yds',
+    'link': 'http://yds.edu.vn'
+  },
+  {
+    'name': 'English Speaking Club H.E.I.',
+    'cssClass': 'hei',
+    'link': 'https://www.facebook.com/engclubhei'
+  },
+  {
+    'name': 'GLN Language',
+    'cssClass': 'gln',
+    'link': 'http://www.gln.edu.vn/vi/'
+  },
+  {
+    'name': 'JOLO Education',
+    'cssClass': 'jolo',
+    'link': 'http://jolo.edu.vn/vi/'
+  },
+  {
+    'name': 'CIPPS',
+    'cssClass': 'cipps',
+    'link': 'https://www.facebook.com/cippsvn/'
+  },
+  {
+    'name': 'Yeah1 - Kênh thông tin giải trí dành cho giới trẻ Việt',
+    'cssClass': 'yeah1',
+    'link': 'http://www.yeah1.com/'
+  }
+]
+
+export default data
